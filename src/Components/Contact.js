8b75a2af5aada330a80c854ec8a360f746a5b446
betobrandaojr <59041231+betobrandaojr@users.noreply.github.com>
2024-02-05 17:25:58 -0300

    Implantação de funcionalidade de envio de email pelo provedor emailjs

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,43 +1,71 @@
 import { useState } from 'react';
 import './Contact.css';
-
+import emailjs from '@emailjs/browser';
 
 function FormEmail() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  function sendEmail(e) {
+    e.preventDefault();
+    if (name === '' || email === '' || message === '') {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const userID = process.env.REACT_APP_EMAILJS_USER_ID;
+
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email
+    };
+
+    emailjs.send(serviceID, templateID, templateParams, userID)
+      .then((response) => {
+        console.log("Email Enviado", response.status, response.text);
+        setName('');
+        setEmail('');
+        setMessage('');
+      }, (err) => {
+        console.log("ERRO: ", err);
+      });
+  }
 
   return (
     <section className='section-form-email-container'>
-        <div className="div-formemail-container" id='contato'>
-            <h1 className="title-formemail">Contato</h1>
-            <form className="form-formemail" onSubmit={() => {}}>
-                <input 
-                className="input-formemail"
-                type="text"
-                placeholder="Digite seu nome"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                />
-                
-                <input 
-                className="input-formemail"
-                type="text"
-                placeholder="Digite seu email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                />
+      <div className="div-formemail-container" id='contato'>
+        <h1 className="title-formemail">Contato</h1>
+        <form className="form-formemail" onSubmit={sendEmail}>
+          <input 
+            className="input-formemail"
+            type="text"
+            placeholder="Digite seu nome"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
 
-                <textarea 
-                className="textarea-formemail"
-                placeholder="Digite sua mensagem..."
-                onChange={(e) => setMessage(e.target.value)}
-                value={message}
-                />
+          <input 
+            className="input-formemail"
+            type="text"
+            placeholder="Digite seu email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
 
-                <input className="button-formemail" type="submit" value="Enviar" />
-            </form>
-        </div>
+          <textarea 
+            className="textarea-formemail"
+            placeholder="Digite sua mensagem..."
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+
+          <input className="button-formemail" type="submit" value="Enviar" />
+        </form>
+      </div>
     </section>
   );
 }
