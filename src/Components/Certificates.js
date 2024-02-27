14a2300571e47ae6b14f873a42233daec9c1e098
betobrandaojr <59041231+betobrandaojr@users.noreply.github.com>
2024-02-27 02:42:45 -0300

    Conclusão etapa de certificados

--- src/Components/Certificates.js
+++ src/Components/Certificates.js
@@ -7,9 +7,32 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-import image1 from '../Assets/img/certificados/JavaScript.png';
-import image2 from '../Assets/img/certificados/vue.png';
-import image3 from '../Assets/img/certificados/scrum.png';
+const images = [
+  { src: require('../Assets/img/certificados/agilidade.png'), alt: 'Agilidade' },
+  { src: require('../Assets/img/certificados/full-stack-1.png'), alt: 'Full Stack 1' },
+  { src: require('../Assets/img/certificados/full-stack-2.png'), alt: 'Full Stack 2' },
+  { src: require('../Assets/img/certificados/full-stack-3.png'), alt: 'Full Stack 3' },
+  { src: require('../Assets/img/certificados/full-stack-4.png'), alt: 'Full Stack 4' },
+  { src: require('../Assets/img/certificados/gestao-agil.png'), alt: 'Gestão Ágil' },
+  { src: require('../Assets/img/certificados/java-script-0.png'), alt: 'JavaScript 0' },
+  { src: require('../Assets/img/certificados/java-script-1.png'), alt: 'JavaScript 1' },
+  { src: require('../Assets/img/certificados/metricas-ageis.png'), alt: 'Métricas Ágeis' },
+  { src: require('../Assets/img/certificados/postgressql-0.png'), alt: 'PostgreSQL 0' },
+  { src: require('../Assets/img/certificados/postgressql-1.png'), alt: 'PostgreSQL 1' },
+  { src: require('../Assets/img/certificados/postgressql-2.png'), alt: 'PostgreSQL 2' },
+  { src: require('../Assets/img/certificados/product-manager.png'), alt: 'Product Manager' },
+  { src: require('../Assets/img/certificados/scrum.png'), alt: 'Scrum' },
+  { src: require('../Assets/img/certificados/startupismo.png'), alt: 'Startupismo' },
+  { src: require('../Assets/img/certificados/team-build.png'), alt: 'Team Building' },
+  { src: require('../Assets/img/certificados/vb-1.png'), alt: 'VB 1' },
+  { src: require('../Assets/img/certificados/vb-2.png'), alt: 'VB 2' },
+  { src: require('../Assets/img/certificados/vb-3.png'), alt: 'VB 3' },
+  { src: require('../Assets/img/certificados/vue-0.png'), alt: 'Vue 0' },
+  { src: require('../Assets/img/certificados/vue-1.png'), alt: 'Vue 1' },
+  { src: require('../Assets/img/certificados/vue-2.png'), alt: 'Vue 2' },
+  { src: require('../Assets/img/certificados/vue-3.png'), alt: 'Vue 3' },
+  { src: require('../Assets/img/certificados/LGPD.png'), alt: 'LGPD' },
+];
 
 function Certificates(){
   return (
@@ -17,31 +40,21 @@ function Certificates(){
       <div className='container-carousel'>
         <h2 className='title-certificates'>Certificados</h2>
         <div className='carousel-overlay'>
-          <Swiper
+        <Swiper
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={50}
             slidesPerView={1}
             navigation
             pagination={{ clickable: true }}
             scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
           >
-            <SwiperSlide>
-              <div className='img-swiper'>
-                <img src={image1} alt='Certificate 1' />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className='img-swiper'>
-                <img src={image2} alt='Certificate 2' />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className='img-swiper'>
-                <img src={image3} alt='Certificate 3' />
-              </div>
-            </SwiperSlide>
+            {images.map((image, index) => (
+              <SwiperSlide key={index}>
+                <div className='img-swiper'>
+                  <img src={image.src} alt={image.alt} />
+                </div>
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </div>
